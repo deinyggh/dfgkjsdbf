c52661329b7045da01790c4924630cd1e34f520d
deinyggh <167228313+deinyggh@users.noreply.github.com>
2024-05-11 08:06:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ server.all('/', (req, res)=>{
     res.send('Your bot is alive!')
 })
 function keepAlive(){
-    server.listen(port, 0.0.0.0, ()=>{console.log("Server is Ready!")});
+    server.listen(port, ()=>{console.log("Server is Ready!")});
 }
 export default keepAlive;
