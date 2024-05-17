c5befa685926a1853f63123b240678fe0cce5884
deinyggh <167228313+deinyggh@users.noreply.github.com>
2024-05-17 23:21:00 +0530

    Add files via upload

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -5,82 +5,105 @@ import { generate_link } from "../buttons/generate_link.js";
 const interactionCreate = async (interaction) => {
 	if (interaction.user.bot) return;
 
-	if (interaction.isChatInputCommand() || interaction.isContextMenuCommand()) {
-		const command = interaction.client.commands.get(interaction.commandName);
-		if (!command) return;
-		const { cooldowns } = interaction.client;
-
-		if (!cooldowns.has(command.data.name)){
-			cooldowns.set(command.data.name, new Collection());
-		}
-
-		const now = Date.now();
-		const timestamps = cooldowns.get(command.data.name);
-		const defaultCooldownDuration = 10;
-		const cooldownAmount = (command.cooldown ?? defaultCooldownDuration) * 1000;
-
-		if (timestamps.has(interaction.user.id)) {
-			const expirationTime = timestamps.get(interaction.user.id) + cooldownAmount;
-
-			if (now < expirationTime) {
-				const expiredTimestamp = Math.round(expirationTime / 1000);
-				return interaction.reply({ content: `Please wait, you are on a cooldown for \`${command.data.name}\`. You can use it again <t:${expiredTimestamp}:R>.`, ephemeral: true });
+	try {
+		if (interaction.isChatInputCommand() || interaction.isContextMenuCommand()) {
+			const command = interaction.client.commands.get(interaction.commandName);
+			if (!command) return;
+			const { cooldowns } = interaction.client;
+	
+			if (!cooldowns.has(command.data.name)){
+				cooldowns.set(command.data.name, new Collection());
+			}
+	
+			const now = Date.now();
+			const timestamps = cooldowns.get(command.data.name);
+			const defaultCooldownDuration = 10;
+			const cooldownAmount = (command.cooldown ?? defaultCooldownDuration) * 1000;
+	
+			if (timestamps.has(interaction.user.id)) {
+				const expirationTime = timestamps.get(interaction.user.id) + cooldownAmount;
+	
+				if (now < expirationTime) {
+					const expiredTimestamp = Math.round(expirationTime / 1000);
+					return interaction.reply({ content: `Please wait, you are on a cooldown for \`${command.data.name}\`. You can use it again <t:${expiredTimestamp}:R>.`, ephemeral: true });
+				}
+			}
+	
+			timestamps.set(interaction.user.id, now);
+			setTimeout(() => timestamps.delete(interaction.user.id), cooldownAmount);
+	
+			try {
+				await command.execute(interaction);
+				console.log(`${interaction.user.username} in #${interaction.channel.name || 'DM'} used ${command.data.name}.`);
+			} catch (error) {
+				console.error(error);
+				try {
+					await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+				} catch {
+					await interaction.editReply({ content: 'There was an error while executing this command!', ephemeral: true });
+				}
 			}
 		}
-
-		timestamps.set(interaction.user.id, now);
-		setTimeout(() => timestamps.delete(interaction.user.id), cooldownAmount);
-
+		else if (interaction.isButton()) {
+			switch (interaction.customId) {
+				case "common":
+				case "pornportal":
+				case "bbnetwork":
+					{
+						await generate_link(interaction);
+					}
+				break;
+	
+				case "guide":
+					{
+						await guides(interaction);
+					}
+				break;
+	
+				case "host_setting":
+					{
+						await host_setting(interaction);
+					}
+				break;
+	
+				default:
+				break;
+			}
+			console.log(`${interaction.user.username} in #${interaction.channel.name || 'DM'} used ${interaction.customId}.`);
+		}
+		else if (interaction.isStringSelectMenu()) {
+			switch (interaction.customId) {
+				case "host_update":
+					{
+						await host_update(interaction);
+					}
+				break;
+	
+				default:
+				break;
+			}
+		}
+		else {
+			return;
+		}
+	} catch (error) {
+		console.log(error);
 		try {
-			await command.execute(interaction);
-			console.log(`${interaction.user.username} in #${interaction.channel.name || 'DM'} used ${command.data.name}.`);
-		} catch (error) {
-			console.error(error);
-			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+			await interaction.reply({ content: `There was an error while executing this command!`, ephemeral: true , components: [], embeds: [
+				new EmbedBuilder()
+				.setColor("DarkRed")
+				.setDescription(`${error}`)
+			] });
+		} catch {
+			await interaction.editReply({ content: `There was an error while executing this command!`, ephemeral: true , components: [], embeds: [
+				new EmbedBuilder()
+				.setColor("DarkRed")
+				.setDescription(`${error}`)
+			] });
 		}
 	}
-	else if (interaction.isButton()) {
-		switch (interaction.customId) {
-			case "common":
-			case "pornportal":
-			case "bbnetwork":
-				{
-					await generate_link(interaction);
-				}
-			break;
 
-			case "guide":
-				{
-					await guides(interaction);
-				}
-			break;
-
-			case "host_setting":
-				{
-					await host_setting(interaction);
-				}
-			break;
-
-			default:
-			break;
-		}
-		console.log(`${interaction.user.username} in #${interaction.channel.name || 'DM'} used ${interaction.customId}.`);
-	}
-	else if (interaction.isStringSelectMenu()) {
-		switch (interaction.customId) {
-			case "host_update":
-				{
-					await host_update(interaction);
-				}
-			break;
-
-			default:
-			break;
-		}
-	}
-	else {
-		return;
-	}
+	
 };
 
 export default {
