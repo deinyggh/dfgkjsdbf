80f4f89e1ec02a8be5651d5574fc9120c309cd30
deinyggh <167228313+deinyggh@users.noreply.github.com>
2024-07-14 21:43:24 +0530

    Add files via upload

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -3,35 +3,35 @@ import { banbros_directory, guides, host_setting, host_update } from "../buttons
 import { generate_link } from "../buttons/generate_link.js";
 
 const interactionCreate = async (interaction) => {
-	if (interaction.user.bot) return;
-
 	try {
+		if (interaction.user.bot) return;
+
 		if (interaction.isChatInputCommand() || interaction.isContextMenuCommand()) {
 			const command = interaction.client.commands.get(interaction.commandName);
 			if (!command) return;
 			const { cooldowns } = interaction.client;
-	
-			if (!cooldowns.has(command.data.name)){
+
+			if (!cooldowns.has(command.data.name)) {
 				cooldowns.set(command.data.name, new Collection());
 			}
-	
+
 			const now = Date.now();
 			const timestamps = cooldowns.get(command.data.name);
 			const defaultCooldownDuration = 10;
 			const cooldownAmount = (command.cooldown ?? defaultCooldownDuration) * 1000;
-	
+
 			if (timestamps.has(interaction.user.id)) {
 				const expirationTime = timestamps.get(interaction.user.id) + cooldownAmount;
-	
+
 				if (now < expirationTime) {
 					const expiredTimestamp = Math.round(expirationTime / 1000);
 					return interaction.reply({ content: `Please wait, you are on a cooldown for \`${command.data.name}\`. You can use it again <t:${expiredTimestamp}:R>.`, ephemeral: true });
 				}
 			}
-	
+
 			timestamps.set(interaction.user.id, now);
 			setTimeout(() => timestamps.delete(interaction.user.id), cooldownAmount);
-	
+
 			try {
 				await command.execute(interaction);
 				console.log(`${interaction.user.username} in #${interaction.channel.name || 'DM'} used ${command.data.name}.`);
@@ -52,28 +52,28 @@ const interactionCreate = async (interaction) => {
 					{
 						await generate_link(interaction);
 					}
-				break;
-	
+					break;
+
 				case "guide":
 					{
 						await guides(interaction);
 					}
-				break;
-	
+					break;
+
 				case "host_setting":
 					{
 						await host_setting(interaction);
 					}
-				break;
+					break;
 
 				case "bangbros_directory":
 					{
 						await banbros_directory(interaction);
 					}
-				break;
-	
+					break;
+
 				default:
-				break;
+					break;
 			}
 			console.log(`${interaction.user.username} in #${interaction.channel.name || 'DM'} used ${interaction.customId}.`);
 		}
@@ -83,10 +83,10 @@ const interactionCreate = async (interaction) => {
 					{
 						await host_update(interaction);
 					}
-				break;
-	
+					break;
+
 				default:
-				break;
+					break;
 			}
 		}
 		else {
@@ -95,21 +95,27 @@ const interactionCreate = async (interaction) => {
 	} catch (error) {
 		console.log(error);
 		try {
-			await interaction.reply({ content: `There was an error while executing this command!`, ephemeral: true , components: [], embeds: [
-				new EmbedBuilder()
-				.setColor("DarkRed")
-				.setDescription(`${error}`)
-			] });
+			await interaction.reply({
+				content: `There was an error while executing this command!`, ephemeral: true, components: [], embeds: [
+					new EmbedBuilder()
+						.setColor("DarkRed")
+						.setDescription(`${error}`)
+				]
+			});
 		} catch {
-			await interaction.editReply({ content: `There was an error while executing this command!`, ephemeral: true , components: [], embeds: [
-				new EmbedBuilder()
-				.setColor("DarkRed")
-				.setDescription(`${error}`)
-			] });
+			try {
+				await interaction.editReply({
+					content: `There was an error while executing this command!`, ephemeral: true, components: [], embeds: [
+						new EmbedBuilder()
+							.setColor("DarkRed")
+							.setDescription(`${error}`)
+					]
+				});
+			} catch (error) {
+				console.log(error);
+			}
 		}
 	}
-
-	
 };
 
 export default {
